fe2dec374e3189aefc6620e4373396f6cf6418ce
eleyna <eleyna@gmail.com>
2013-06-18 12:09:43 -0400

    Madlibs story

---
+++ madlibs.rb
@@ -0,0 +1,24 @@
+p "Give me a noun: "
+noun_one = gets.chomp
+p "Give me a noun: "
+noun_two = gets.chomp
+p "Give me a verb: "
+verb_one = gets.chomp
+p "Give me a noun: "
+noun_three = gets.chomp
+p "Give me a verb: "
+verb_two = gets.chomp
+p "Give me a adjective: "
+adjective_one = gets.chomp
+p "Give me a noun: "
+noun_four = gets.chomp
+p "Give me a verb: "
+verb_three = gets.chomp
+p "Give me a noun: "
+noun_five = gets.chomp
+p "Give me a verb: "
+verb_four = gets.chomp
+
+p "Today I went to the #{noun_one}. When I was at the #{noun_two}, I #{verb_one} a " +
+  "#{noun_three} and then I #{verb_two}. Afterwards, my #{adjective_one} #{noun_four}" +
+  "#{verb_three} a #{noun_five} and #{verb_four}."
